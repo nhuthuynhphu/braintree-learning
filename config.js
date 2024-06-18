e8c4ca7c977c5691a08f3fa320879de7d05618a1
huynhphu <huynh.phu@siliconstack.com.au>
2024-06-18 09:28:25 +0700

    refactor

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const braintree = require('braintree');
 
 const gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox, // Use Sandbox for testing
-  merchantId: 'ycjkrz224yt442t3',
-  publicKey: 'zwdgxrkq6tphsdzx',
-  privateKey: 'd98ca01931d7fd1da73a8d1be1dd9dd4',
+  merchantId: '',
+  publicKey: '',
+  privateKey: '',
 });
 
 module.exports = gateway;
